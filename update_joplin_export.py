cfa722660b2cf1d81a6fc568680f4a2c703cd856
nilsnevertree <nils.niebaum@gmx.de>
2024-02-25 12:20:51 +0100

    Update Joplin export script and modify links to _resources

--- update_joplin_export.py
+++ update_joplin_export.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from typing import Union
 import shutil
 import re
-
+from bs4 import BeautifulSoup as bs
 import argparse
 
 def findReplace(
@@ -49,30 +49,80 @@ def findReplace(
             with open(filepath, "w") as f:
                 f.write(s)
 
+def findReplaceRelativePath(
+        directory : Union[os.PathLike, str], 
+        filePattern : str
+        ) -> None:
+    """Recursively find and replace text in files in a directory.
+    
+    Parameters
+    ----------
+    directory : Union[os.PathLike, str]
+        The directory to search in.
+    filePattern : str
+        The file pattern to search for.
+    
+    Returns
+    -------
+    None
+    """
 
+    # Regex for the full substring
+    pattern_full = r"(\.\./)*_resources/([a-zA-Z0-9])+\.(png|jpg|gif)"
+    regex_full = re.compile(pattern_full)
+    # regex for the _resources pattern
+    pattern_resource = r"(\.\./)*_resources"
+    regex_resource = re.compile(pattern_resource, re.IGNORECASE)
 
+    for path, dirs, files in os.walk(os.path.abspath(directory)):
+        for filename in fnmatch.filter(files, filePattern):
+            filepath = os.path.join(path, filename)
+            print(filepath)
+            with open(filepath) as f:
+                s = f.read()
+            # Extract the full substring from the original string
+            lines = []
+            for line in s.split("\n"):
+                match = regex_full.search(line)
+                if match is not None:
+                    name = match.group(0)
+                    # Replace the _resources with the new path
+                    name = regex_resource.sub('{{"assets/images', name)
+                    # Add the end of the string
+                    name += '" | relative_url }}'
+                    # Replace the full substring with the new path
+                    line = regex_full.sub(name, line)
+                else:
+                    pass
+                lines.append(line)
+            res = "\n".join(lines)
+            with open(filepath, "w") as f:
+                f.write(res)
+#%%
 # Create an ArgumentParser object
-parser = argparse.ArgumentParser(description='Update Joplin export')
+# parser = argparse.ArgumentParser(description='Update Joplin export')
 
-# Add arguments
-parser.add_argument('--origin', type=str, help='Origin directory', default=r"C:\Users\Niebaum\Desktop\joplin_export")
-parser.add_argument('--destination', type=str, help='Destination directory', default=r"C:\Users\Niebaum\Documents\Repositories\sdm-eurec4a-notes")
+# # Add arguments
+# parser.add_argument('--origin', type=str, help='Origin directory', default=r"C:\Users\Niebaum\Desktop\joplin_export")
+# parser.add_argument('--destination', type=str, help='Destination directory', default=r"C:\Users\Niebaum\Documents\Repositories\sdm-eurec4a-notes")
 
-# Parse the arguments
-args = parser.parse_args()
+# # Parse the arguments
+# args = parser.parse_args()
 
-# Define the directories
-origin_dir = Path(args.origin)
-destination_dir = Path(args.destination)
+# # Define the directories
+# origin_dir = Path(args.origin)
+# destination_dir = Path(args.destination)
 # #%%
+origin_dir = Path(r"C:\Users\Niebaum\Desktop\joplin_export")
+destination_dir = Path(r"C:\Users\Niebaum\Documents\Repositories\sdm-eurec4a-notes")
 
-if input(f"\n- Origin: {origin_dir}"
-      f"\n- Destination: {destination_dir}"
-      "\nAre these correct directories? [yes/no]\n"
-      ) in ["yes", "Yes", "YES", "y", "Y"]:
-    pass
-else:
-    raise KeyboardInterrupt("User cancelled the operation.")
+# if input(f"\n- Origin: {origin_dir}"
+#       f"\n- Destination: {destination_dir}"
+#       "\nAre these correct directories? [yes/no]\n"
+#       ) in ["yes", "Yes", "YES", "y", "Y"]:
+#     pass
+# else:
+#     raise KeyboardInterrupt("User cancelled the operation.")
 
 
 print(f"\nCopying the files from origin_dir to destination_dir...")
@@ -120,17 +170,7 @@ findReplace(
     filePattern="*.html"
 )
 
-# Modify the links to the _resources
-print(f"\nModifying the links to the _resources ...")
-pattern = r"(\.\./)*_resources/"
-regex = re.compile(pattern, re.IGNORECASE)
-findReplace(
-    directory=new_html_dir,
-    find=regex,
-    replace="https://nilsnevertree.github.io/sdm-eurec4a-notes/assets/images/",
-    filePattern="*.html"
-)
-
+#%%
 # Remove the header string from the html files
 print(f"\nRemoving the header string from the html files ...")
 header_str = "<!DOCTYPE html>"
@@ -140,5 +180,27 @@ findReplace(
     replace="",
     filePattern="*.html"
 )
+#%%
+# # Prettyfy html
+# # !!!!!!!! THIS NEEDS TO BE DONE FIRST !!!!!!!!!
+# from bs4 import BeautifulSoup as bs
+# for path, dirs, files in os.walk(os.path.abspath(new_html_dir)):
+#     for filename in fnmatch.filter(files, '*.html'):
+#         filepath = os.path.join(path, filename)
+#         with open(filepath) as f:
+#             s = f.read()
+#         soup = bs(s, features="html.parser")                #make BeautifulSoup
+#         prettyHTML = soup.prettify(formatter="html")   #prettify the html
+#         with open(filepath, "w") as f:
+#             f.write(prettyHTML)
 
+
+#%%
+# Modify the links to the _resources
+print(f"\nModifying the links to the _resources ...")
+
+findReplaceRelativePath(
+    directory=new_html_dir,
+    filePattern="*.html"
+)
 # %%
